e6981ddae45b24b6456eabac54c1001c8631cf7e
Rafael Girão <git@rafael.ovh>
2023-09-08 17:46:45 +0100

    Switch flake.nix structure to flake.parts template

--- flake.nix
+++ flake.nix
@@ -1,32 +1,62 @@
 {
-  description = "Python example flake for Zero to Nix";
+  description = "Description for the project";
 
   inputs = {
     nixpkgs.url = "flake:nixpkgs";
+    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      nameValuePair = name: value: { inherit name value; };
-      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
-      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f: genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      packages = forAllSystems ({ pkgs }: {
-        default =
-          let
-            python = pkgs.python39;
-          in
-          python.pkgs.buildPythonApplication {
-            name = "bolsas-scraper";
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+        inputs.devenv.flakeModule
 
-            propagatedBuildInputs = with python.pkgs; [ pip requests beautifulsoup4 wget lxml ];
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          python = pkgs.python3;
+        in
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
 
-            src = ./.;
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+          packages.default =
+            let
+              python = pkgs.python3;
+            in
+            python.pkgs.buildPythonApplication {
+              name = "bolsas-scraper";
+
+              propagatedBuildInputs = with python.pkgs;  [ requests wget beautifulsoup4 lxml ];
+
+              src = ./.;
+            };
+
+          devenv.shells.default = {
+            languages.python = {
+              enable = true;
+              package =  (pkgs.python3.withPackages (ps: with ps; with pkgs.python3Packages; [
+                  requests
+                  wget
+                  lxml
+                  beautifulsoup4
+            ]));
+            };
+            dotenv.enable = true;
           };
-      });
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
     };
 }
